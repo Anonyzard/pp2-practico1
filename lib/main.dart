2b658d1db255fe886f17101eeb70913ca105635f
Kevin Guevara <103538695+monchoK@users.noreply.github.com>
2022-08-28 18:07:27 -0300

    Update main.dart
    puse los temas

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,24 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Login(),
+      theme: ThemeData(
+          backgroundColor: const Color.fromARGB(255, 201, 117, 21),
+          primaryColor: Colors.grey.shade300,
+          secondaryHeaderColor: Colors.orange.shade100,
+          textTheme: const TextTheme(
+              headline1: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black), //NOMBRE DE LOS PRODUCTOS Y PRECIO
+              headline2: TextStyle(
+                  fontSize: 10,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.black),
+              headline3: TextStyle(
+                  fontSize: 17,
+                  color: Colors.black,
+                  fontStyle: FontStyle.normal) //BOTON COMPRAR DEL CATALOGO
+              )),
     );
   }
 }
