8821e4f8aeae8ff2a7b021e86c032269f4324178
José Leiva <penachoman5@gmail.com>
2022-08-28 18:21:41 -0300

    Conectando login con mainscreen

--- lib/login.dart
+++ lib/login.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:proyecto1practica2/signin.dart';
+import 'package:proyecto1practica2/pages/main_screen.dart';
 
 void main() => runApp(Login());
 
@@ -79,18 +80,17 @@ class Login extends StatelessWidget {
               // style: ButtonStyle(
               //     backgroundColor: Color.fromARGB(255, 201, 117, 21)), //,
               onPressed: () {
-                // Validate returns true if the form is valid, or false otherwise.
-                if (_nombre.currentState!.validate()) {
-                  // If the form is valid, display a snackbar. In the real world,
-                  // you'd often call a server or save the information in a database.
-                  ScaffoldMessenger.of(context).showSnackBar(
-                    const SnackBar(content: Text('Processing Data')),
-                  );
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => Signin()),
-                  );
-                }
+                // // Validate returns true if the form is valid, or false otherwise.
+                // if (_nombre.currentState!.validate()) {
+                //   // If the form is valid, display a snackbar. In the real world,
+                //   // you'd often call a server or save the information in a database.
+                //   ScaffoldMessenger.of(context).showSnackBar(
+                //     const SnackBar(content: Text('Processing Data')),
+                //   );
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => main_screens()),
+                );
               },
               child: const Text('Iniciar sesión'),
             ),
